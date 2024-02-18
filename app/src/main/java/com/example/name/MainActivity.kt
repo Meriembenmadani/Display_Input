89d2607a020871e05a3c2e984e31c0f5f3fe2a5f
Meriembenmadani <m_benmadani@estin.dz>
2024-02-18 21:57:22 +0100

    finished project

--- app/src/main/java/com/example/name/MainActivity.kt
+++ app/src/main/java/com/example/name/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+//import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -14,13 +14,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
+//import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
+//import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.MaterialTheme
+//import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -30,10 +30,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+//import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.painter.Painter
+//import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -44,9 +44,9 @@ import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.name.R.*
+//import com.example.name.R.*
 import com.example.name.ui.theme.NameTheme
-import com.example.name.ui.theme.Poppins
+//import com.example.name.ui.theme.Poppins
 import com.example.name.ui.theme.PoppinsSemi
 
 class MainActivity : ComponentActivity() {
@@ -134,10 +134,10 @@ fun MyScreen() {
 
 @Preview
 @Composable
-public  fun Enter() {
+fun Enter() {
     Card(
         shape = RectangleShape,
-        colors = CardDefaults.cardColors(containerColor = Color.White,),
+        colors = CardDefaults.cardColors(containerColor = Color.White),
         modifier = Modifier.size(317.dp, 209.dp),
 
 
